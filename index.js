c35d3d77984d29d2ac508faa22dc0c5c27def6e4
tovernaar123 <ben.schellevis@outlook.com>
2020-08-28 12:03:13 +0200

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+let root_path = path.join(__dirname, 'site_files')
+const fs = require('fs');
+
+app.get('*', function (req, res, next) {
+    var options = {
+      root: root_path,
+      dotfiles: 'deny',
+      headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
+    }
+  
+    var fileName = decodeURIComponent(req.url)
+    if(fileName === '/Home.html'){fileName = 'index.html'}
+    console.log(req)
+    if(fs.existsSync(path.join(root_path, fileName))) {
+        res.sendFile(fileName, options, function (err) {
+            if (err) {
+                next(err)
+            } else {
+                console.log('Sent:', fileName)
+            }
+        })
+    }else{
+        res.send(403, 'not avaible');
+    }
+});
+
+
+app.post('*', function (req, res, next) {
+    res.send('ok')
+})
+ 
+app.listen(3000)
